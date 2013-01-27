689e616d300e4656d728c547b1f44742536c37b9
Ty Pobe <pax10@mobileinfern.us>
2013-01-26 19:26:39 -0800

    edit version
    until I get a better way to grab device this is it.
    
     Changes to be committed:
    	modified:   configs/common_versions.mk

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,20 +2,12 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 DATE = $(shell vendor/pax/tools/getdate)
-DEVICE="toro"
-PAX_BUILD=raum00_$(DEVICE)-4.2.1-$(DATE)
 
-ifneq ($(PAX_BUILD),)
-	# PAX_BUILD=<goo version int>/<build string>
-	PRODUCT_PROPERTY_OVERRIDES += \
-	    ro.goo.developerid=pax \
-	    ro.goo.rom=raum00 \
-	    ro.goo.version=raum00_$(DEVICE)-4.2.1-$(DATE) \
-	    ro.raum.version=raum00_$(DEVICE)-4.2.1-$(DATE)
-else
-	PRODUCT_PROPERTY_OVERRIDES += \
-	    ro.raum.version=raum00_unofficial-4.2.1-$(DATE)
-endif
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.developerid=pax \
+    ro.goo.rom=raum00 \
+    ro.goo.version=raum00-4.2.1-$(DATE) \
+    ro.raum.version=raum00-$(DATE)
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
